3ea8912bb8116338781a8bd6853f429f49ccd979
Orlando <rakeecao@gmail.com>
2022-05-16 20:54:42 +0800

    增加个人中心

--- src/locales/lang/zh-CN/routes/account.ts
+++ src/locales/lang/zh-CN/routes/account.ts
@@ -1,3 +1,6 @@
 export default {
   account: '个人中心',
+  setting: '设置',
+  doc: '框架文档',
+  company: '公司网站',
 };
--- src/router/routes/modules/account.ts
+++ src/router/routes/modules/account.ts
@@ -1,5 +1,6 @@
 import type { AppRouteModule } from '/@/router/types';
 
+const IFrame = () => import('/@/views/sys/iframe/FrameBlank.vue');
 import { LAYOUT } from '/@/router/constant';
 import { t } from '/@/hooks/web/useI18n';
 
@@ -9,7 +10,7 @@ const account: AppRouteModule = {
   component: LAYOUT,
   redirect: '/account/index',
   meta: {
-    hideChildrenInMenu: true,
+    // hideChildrenInMenu: true,
     icon: 'bx:cog',
     title: t('routes.account.account'),
     orderNo: 12,
@@ -20,9 +21,29 @@ const account: AppRouteModule = {
       name: 'AccountPage',
       component: () => import('/@/views/account/index.vue'),
       meta: {
-        title: t('routes.account.account'),
-        icon: 'simple-icons:about-dot-me',
-        hideMenu: true,
+        title: t('routes.account.setting'),
+        icon: 'bx:cog',
+        // hideMenu: true,
+      },
+    },
+    {
+      path: 'doc',
+      name: 'Doc',
+      component: IFrame,
+      meta: {
+        frameSrc: 'https://vvbin.cn/doc-next/',
+        title: t('routes.account.doc'),
+        icon: 'bx:dock-left',
+      },
+    },
+    {
+      path: 'company',
+      name: 'Company',
+      component: IFrame,
+      meta: {
+        frameSrc: 'https://app.shkqg.com/',
+        title: t('routes.account.company'),
+        icon: 'logos:bigpanda',
       },
     },
   ],
